08668e204119a8164fb3ff78f7ca57507fcd2cd0
Sergey <Zaikouski.sergey@gmail.com>
2023-09-15 12:48:07 +0300

    feat: add form

--- src/components/auth/edit-form/edit-form.tsx
+++ src/components/auth/edit-form/edit-form.tsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { FC, useState } from 'react'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import styles from './edit-form.module.scss'
 
-import { Button, Typography } from '@/components'
+import { Button, TextField, Typography } from '@/components'
 import { Avatar } from '@/components/ui/avatar/avatar.tsx'
 import { Card } from '@/components/ui/card/card.tsx'
 import { Logout } from '@/svg'
 import { Edit2Outline } from '@/svg/edit-2-outline.tsx'
 
-const EditForm = () => {
+const nameSchema = z.object({
+  nickname: z.string().nonempty('enter name').default(''),
+})
+
+type nameFormSchema = z.infer<typeof nameSchema>
+
+type EditFormProps = {
+  contactInfo?: { name: string; email: string; avatar: string }
+}
+
+const EditForm: FC<EditFormProps> = ({}) => {
   const [editInfo, setEditInfo] = useState<boolean>(false)
+
+  const currentName = 'Ivanio'
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<nameFormSchema>({
+    resolver: zodResolver(nameSchema),
+    defaultValues: { nickname: '' },
+  })
+  const onSubmit = (data: nameFormSchema) => {
+    console.log(data)
+    setEditInfo(false)
+  }
 
   return (
     <Card>
@@ -28,7 +57,7 @@ const EditForm = () => {
       {!editInfo ? (
         <div className={styles.editInfo}>
           <div className={styles.editBlock}>
-            <Typography variant={'h1'}>Ivan</Typography>
+            <Typography variant={'h1'}>{currentName}</Typography>
             <button
               onClick={() => {
                 setEditInfo(true)
@@ -47,7 +76,24 @@ const EditForm = () => {
           </Button>
         </div>
       ) : (
-        <div></div>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <Controller
+            name="nickname"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                value={field.name}
+                onChange={field.onChange}
+                onBlur={field.onBlur}
+                label={'Nickname'}
+                error={errors.nickname?.message}
+              />
+            )}
+          />
+          <Button type="submit" variant={'primary'} fullWidth={true}>
+            Save Changes
+          </Button>
+        </form>
       )}
     </Card>
   )
