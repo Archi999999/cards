e29f97a34ce9fb174bc509c162f859621938440c
Archi99999 <archi999999@gmail.com>
2023-10-11 13:17:09 +0300

    fix: stories header & slider

--- src/components/ui/slider/slider.stories.tsx
+++ src/components/ui/slider/slider.stories.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
+
 import { Slider } from './slider.tsx'
 
 export default {
   title: 'Components/Slider',
   component: Slider,
 }
-export const CustomValues = () => <Slider minValue={10} maxValue={90} />
+export const CustomValues = () => {
+  const [currentMinValue, setCurrentMinValue] = useState(0)
+  const [currentMaxValue, setCurrentMaxValue] = useState(90)
+  const maxValue = 90
+
+  return (
+    <Slider
+      maxValue={maxValue}
+      currentMaxValue={currentMaxValue}
+      currentMinValue={currentMinValue}
+      label={'Label Slider'}
+      valueChangeHandler={values => {
+        setCurrentMinValue(values[0])
+        setCurrentMaxValue(values[1])
+      }}
+    />
+  )
+}
