21411dd114b714c32b2e70d1053a86492ba57316
Sergey <Zaikouski.sergey@gmail.com>
2023-10-20 18:15:47 +0300

    feat: learn-pack fix

--- src/pages/learn-pack/learn-pack.tsx
+++ src/pages/learn-pack/learn-pack.tsx
@@ -4,10 +4,17 @@ import styles from './learn-pack.module.scss'
 
 import { Typography } from '@/components'
 import { Card } from '@/components/ui/card/card.tsx'
+import { useGetRandomCardQuery } from '@/services/cards/cards.ts'
+import { useGetDeckByIdQuery } from '@/services/decks/decks.ts'
 import { ArrowBack } from '@/svg/arrow-back-outline.tsx'
 
 export const LearnPack = () => {
   const { deckId } = useParams<{ deckId: string }>()
+  const { data } = useGetDeckByIdQuery({
+    id: deckId!,
+  })
+
+  const { data: dataRandomCard } = useGetRandomCardQuery({ idDeck: deckId || '' })
 
   return (
     <div className={styles.wrapper}>
@@ -16,7 +23,12 @@ export const LearnPack = () => {
         <Typography variant={'body_2'}> Back to Packs List</Typography>
       </Link>
       <Card>
-        <Typography variant={'large'}>Learn “Pack Name”{deckId}</Typography>
+        <Typography variant={'large'}>Learn “{data ? data.name : ''}”</Typography>
+        <Typography variant={'subtitle_1'}>Question:</Typography>
+        <div>{dataRandomCard && dataRandomCard.question}</div>
+        <Typography variant={'body_2'}>
+          Количество попыток ответов на вопрос: {dataRandomCard && dataRandomCard.shots}
+        </Typography>
       </Card>
     </div>
   )
