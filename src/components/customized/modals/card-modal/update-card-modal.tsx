0fa2b214e32c66e92d22f652ea1576a107a06e5c
Sergey <Zaikouski.sergey@gmail.com>
2023-11-03 15:00:50 +0200

    add validation in update-card-modal.tsx

--- src/components/customized/modals/card-modal/update-card-modal.tsx
+++ src/components/customized/modals/card-modal/update-card-modal.tsx
@@ -1,5 +1,7 @@
 import { FC, useState } from 'react'
 
+import { toast, ToastContainer } from 'react-toastify'
+
 import { TextField } from '@/components'
 import { Modal } from '@/components/ui/modal/modal.tsx'
 import { useUpdateCardMutation } from '@/services/cards/cards.ts'
@@ -18,24 +20,33 @@ export const UpdateCardModal: FC<Props> = ({ setModal, cardId, answerCard, quest
   const [updateCard] = useUpdateCardMutation()
   const confirmUpdateCard = () => {
     updateCard({ id: cardId, answer: answer, question: question })
-    setModal(false)
+      .unwrap()
+      .then(() => {
+        setModal(false)
+      })
+      .catch(() => {
+        toast('question and answer must be longer than or equal to 3 characters"')
+      })
   }
 
   return (
-    <Modal
-      title={'Edit card'}
-      setModal={setModal}
-      confirmButtonName={'Save Changes'}
-      onConfirm={confirmUpdateCard}
-    >
-      {/*<SelectSecond*/}
-      {/*  defaultValue={options[0].value}*/}
-      {/*  label={'Choose a question format'}*/}
-      {/*  className={s.select}*/}
-      {/*  options={options}*/}
-      {/*/>*/}
-      <TextField label={'Question'} value={question} onValueChange={setQuestion} />
-      <TextField label={'Answer'} value={answer} onValueChange={setAnswer} />
-    </Modal>
+    <>
+      <ToastContainer autoClose={2000} position="top-center" limit={2} />
+      <Modal
+        title={'Edit card'}
+        setModal={setModal}
+        confirmButtonName={'Save Changes'}
+        onConfirm={confirmUpdateCard}
+      >
+        {/*<SelectSecond*/}
+        {/*  defaultValue={options[0].value}*/}
+        {/*  label={'Choose a question format'}*/}
+        {/*  className={s.select}*/}
+        {/*  options={options}*/}
+        {/*/>*/}
+        <TextField label={'Question'} value={question} onValueChange={setQuestion} />
+        <TextField label={'Answer'} value={answer} onValueChange={setAnswer} />
+      </Modal>
+    </>
   )
 }
