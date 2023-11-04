8a0b650d30189eb194496a5e8a3b37f2b79dbb1f
Archi999999 <archi999999@gmail.com>
2023-11-04 15:31:50 +0300

    feat: card password-recovery UI

--- src/components/auth/password-recovery/password-recovery.tsx
+++ src/components/auth/password-recovery/password-recovery.tsx
@@ -1,16 +1,27 @@
 import s from './password-recovery.module.scss'
 
-import { Typography } from '@/components'
+import {Button, TextField, Typography} from '@/components'
 import { Card } from '@/components/ui/card/card.tsx'
 
 export const PasswordRecovery = () => {
   return (
-    <div className={s.recovery}>
       <Card>
-        <Typography variant={'large'} as={'h1'}>
+        <Typography variant={'large'} as={'h1'} className={s.title}>
           Forgot your password?
         </Typography>
+        <TextField label={'Email'} />
+        <Typography className={s.textInfo}>
+          Enter your email address and we will send you further instructions
+        </Typography>
+        <Button>
+          Send Instructions
+        </Button>
+        <div className={s.reminder}>
+          <Typography className={s.textReminder}>
+            Did you remember your password?
+          </Typography>
+          <Button variant={'link'}>Try logging in</Button>
+        </div>
       </Card>
-    </div>
   )
 }
